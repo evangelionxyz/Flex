53a6ef29594dea54cbc9e457ebba3e3b83465129
Evangelion Manuhutu <evangelion11.m@gmail.com>
2025-11-03 14:24:09 +0700

    Preparing scene management

---
+++ Flex/Source/Scene/Entity.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2025 Flex Engine | Evangelion Manuhutu
+
+#ifndef ENTITY_H
+#define ENTITY_H
+
+namespace flex
+{
+	class Entity
+	{
+	public:
+
+	};
+}
+
+#endif
---
+++ Flex/Source/Scene/Serializer.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2025 Evangelion Manuhutu
+
+#ifndef SERIALIZER_H
+#define SERIALIZER_H
+
+namespace flex
+{
+	class Serializer
+	{
+	public:
+
+	};
+}
+
+#endif
